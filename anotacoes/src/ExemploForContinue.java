89da5a7e900ab374a183173d491e6a51f257317a
leoeblt <leonardobprandi@gmail.com>
2024-05-15 17:08:32 -0300

    l

--- anotacoes/src/ExemploForContinue.java
+++ anotacoes/src/ExemploForContinue.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class ExemploForContinue {
 
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Escrevendo i: " + i);
+            if (i < 10) {
+                continue;
+            }
+            System.out.println("Escrevendo algo");
+            System.out.println("Escrevendo mais");
+        }
+    }
 }
--- unidade5/src/exercicio6.java
+++ unidade5/src/exercicio6.java
@@ -6,9 +6,22 @@ public class exercicio6 {
 
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-
         
-
+        int quantidadePessoas = 3;
+        
+        double somaAlturas = 0.0;
+        
+        for (int i = 1; i <= quantidadePessoas; i++) {
+            System.out.print("Digite a altura da pessoa " + i + " (em metros): ");
+            double altura = teclado.nextDouble();
+            somaAlturas += altura;
+        }
+        
+        double mediaAlturas = somaAlturas / quantidadePessoas;
+        
+        System.out.println("A média das alturas das " + quantidadePessoas + " pessoas é: " + mediaAlturas + " metros.");
+        
+        teclado.close();
 
     }
 
