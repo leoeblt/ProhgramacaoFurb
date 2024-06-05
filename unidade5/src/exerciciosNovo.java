f42af81a1f2ef47b46b72ecccd3ea7c14ca8a651
leoeblt <leonardobprandi@gmail.com>
2024-06-05 17:12:56 -0300

    l

--- unidade5/src/exerciciosNovo.java
+++ unidade5/src/exerciciosNovo.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class exerciciosNovo {
 
-    // Descreva um algoritmo que leia 20 números inteiros e escreva, para cada número lido, se o mesmo é par ou ímpar.
+    // 1- Descreva um algoritmo que leia 20 números inteiros e escreva, para cada número lido, se o mesmo é par ou ímpar.
 
     // public static void main(String[] args) {
     //     Scanner teclado = new Scanner(System.in);
@@ -30,31 +30,68 @@ public class exerciciosNovo {
     
 //========================================================================================================================================//
 
-    //Descreva um algoritmo que calcule e escreva a soma dos números pares e a soma dos números ímpares entre 1 e 100.
+    // 2- Descreva um algoritmo que calcule e escreva a soma dos números pares e a soma dos números ímpares entre 1 e 100.
 
-        public static void main(String[] args) {
-            Scanner teclado = new Scanner(System.in);
+        // public static void main(String[] args) {
 
-            int numerosPares = 0;
-            int numerosImpares = 0;
+        //     int numerosPares = 0;
+        //     int numerosImpares = 0;
             
-            for (int i = 1; i <= 100; i++) {
-                int numInteiro = i;
-                if (numInteiro %2 == 0) {
-                    numerosPares += numInteiro;
-                } else {
-                    numerosImpares += numInteiro;
-                }
+        //     for (int i = 1; i <= 100; i++) {
+        //         int numInteiro = i;
+        //         if (numInteiro %2 == 0) {
+        //             numerosPares += numInteiro;
+        //         } else {
+        //             numerosImpares += numInteiro;
+        //         }
 
-            }
+        //     }
 
-            System.out.println(numerosPares);
-            System.out.println(numerosImpares);
+        //     System.out.println(numerosPares);
+        //     System.out.println(numerosImpares);
 
-            teclado.close();
+        // }
 
+    
+
+//=======================================================================================================================================//
+
+    // 3- Descreva um algoritmo que calcule e escreva a soma da seguinte série de 100 termos: 
+    // 1/1 + 1/2 + 1/3 ... 1/100
+    
+    // public static void main(String[] args) {
+    //     Scanner teclado = new Scanner(System.in);    
+        
+    //     double soma = 0.0;
+
+    //     for (int i = 1; i <= 100; i++) {
+    //         soma += 1.0/i;
+    //     }
+
+    //     System.out.printf("A soma da série é: %f", soma);
+    
+    // }
+
+//======================================================================================================================================//
+
+    // 6- Descreva um algoritmo que leia a altura de 20 pessoas e calcule a média de altura das mesmas.
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        double somaAlturas = 0.0;
+
+        for (int i = 1; i <= 4; i++) {
+            System.out.println("Informe a altura das pessoas");
+            double altura = teclado.nextDouble();
+            somaAlturas += altura;
         }
+
+        double mediaAlturas = somaAlturas / 4;
+
+        System.out.printf("A média das alturas das pessoas é: %f", mediaAlturas);
+
+        teclado.close();
 
     }
 
-//=======================================================================================================================================//
+}
